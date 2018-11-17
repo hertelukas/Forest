edd5476c013d9c26675205ae015f843b9e021ce5
hertelukas <luki.hertel@gmail.com>
2018-11-18 00:17:37 +0100

    First Commit
    Added Unity Project

---
+++ Assets/PlayerAnimation.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour {
+
+    public Animator animator;
+    public float walking;
+    public float turning;
+    public float jump;
+    public float jumpLeg;
+    public bool crouch = false;
+
+    float horizontalSpeed = 2.0f;
+    float verticalSpeed = 2.0f;
+
+    private Vector3 rotateValue;
+
+    public Camera Camera;
+
+    void Start ()
+    {
+        animator = this.gameObject.GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        float h = horizontalSpeed * Input.GetAxis("Mouse X");
+        float v = -0.5f * (verticalSpeed * Input.GetAxis("Mouse Y"));
+        //v = Mathf.Clamp(v, -150, 150);
+
+        rotateValue = new Vector3(v, 0, 0);
+        Camera.transform.Rotate (rotateValue);
+       
+        walking = Input.GetAxis("Vertical") / 2;
+        turning = h;
+
+
+        if (Input.GetKey(KeyCode.Y))
+        {
+            crouch = true;
+            walking = walking / 3;
+        }
+        else
+        {
+            crouch = false;
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            walking = walking * 2;
+        }
+
+        jump = 0;
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            jump = 5f;
+        }
+
+
+        animator.SetFloat("Forward", Mathf.Abs(walking));
+        //animator.SetFloat("Turn", turning);
+        animator.SetBool("Crouch", crouch);
+        animator.SetFloat("Jump", jump);
+        animator.SetFloat("JumpLeg", jumpLeg);
+
+
+        transform.Translate (0f, 0f, walking * 10 * Time.deltaTime);
+        transform.Rotate(0f, turning, 0f);
+	}
+}
---
+++ Assets/SceneManagement.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using System.Collections;
+
+public class SceneManagement : MonoBehaviour {
+
+    public GameObject settingsUI;
+    public GameObject PauseMenuUI;
+    int i;
+    bool settingsUIOpened = false;
+
+
+
+    private void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (settingsUIOpened == false)
+            {
+                OpenSettingsUI();
+            }
+            /*
+            if (settingsUIOpened == true)
+            {
+                CloseSettingsUI();
+            }*/
+        }
+
+        if (Input.GetKey(KeyCode.Tab))
+        {
+            OpenPauseMenuUI();
+        }
+        else
+        {
+            ClosePauseMenuUI();
+        }
+    }
+
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene("Menu");
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void LoadSettings()
+    {
+        SceneManager.LoadScene("Settings");
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    void OpenSettingsUI()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        settingsUI.SetActive(true);
+        settingsUIOpened = true;
+    }
+
+    public void CloseSettingsUI()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        settingsUI.SetActive(false);
+        settingsUIOpened = false;
+    }
+
+    void OpenPauseMenuUI()
+    {
+        PauseMenuUI.SetActive(true);
+    }
+
+    void ClosePauseMenuUI()
+    {
+        PauseMenuUI.SetActive(false);
+    }
+}
